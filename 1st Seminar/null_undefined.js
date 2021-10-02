e91319055a20295782f2a6bc907b170d9b6ed0a9
Danna <daeunlee0720@gmail.com>
2021-10-02 17:10:51 +0900

    6. null vs. undefined

--- 1st Seminar/null_undefined.js
+++ 1st Seminar/null_undefined.js
@@ -8,10 +8,3 @@ console.log(typeof null); // object? -> JS의 버그
 // null vs undefined
 console.log("null == undefined ", null == undefined); // true
 console.log("null === undefined ", null === undefined); // false
-
-// truthy falsy
-// 대충 트루다 대충 폴스다
-console.log("1" == true) // true
-console.log("2" == true) // false
-
-// falsy: false, 0, null, undefined, ''
