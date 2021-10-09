7458bdf6893ce6b1b3caafe37854b5476d602514
Danna <daeunlee0720@gmail.com>
2021-10-09 17:00:46 +0900

    여러 개의 모듈 calculator 실습

---
+++ 2nd Seminar/main_calculator.js
@@ -0,0 +1,6 @@
+const calculator = require("./calculator");
+
+console.log("sum result:", calculator.add(1, 3));
+console.log("sum result:", calculator.subtract(1, 3));
+console.log("sum result:", calculator.multiply(1, 3));
+console.log("sum result:", calculator.divide(1, 3));
