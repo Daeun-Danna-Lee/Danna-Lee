c51a823dc2795fc471a2f95ff7993482105e3e03
Danna <daeunlee0720@gmail.com>
2021-10-30 03:39:32 +0900

    [feat] user router 생성

---
+++ 3rd Seminar/crud/routes/user.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+
+// user/signup
+// router.[method]("[route]", [logic])
+router.post("/signup", (req, res) => {
+    
+});
+
+module.exports = router;
