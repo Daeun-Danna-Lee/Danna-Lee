ebc6762d1d2df6db50adb7ccdc82712d079c459f
Danna <daeunlee0720@gmail.com>
2021-11-13 14:29:55 +0900

    3차 세미나 index, user.js

--- 3rd Seminar/crud/routes/user.js
+++ 3rd Seminar/crud/routes/user.js
@@ -1,10 +1,19 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // user/signup
 // router.[method]("[route]", [logic])
 router.post("/signup", (req, res) => {
     
+    // email, password
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+
+    const newUser = { name: name, password: password, email: email };
+
+    res.status(200).send(newUser);
+
 });
 
 module.exports = router;
