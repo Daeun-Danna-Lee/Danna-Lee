e824d9919fc4bde773ae507fc4a633b9001be573
Danna <daeunlee0720@gmail.com>
2021-11-13 15:05:43 +0900

    [dev] firebase key

--- 4th Seminar/functions/index.js
+++ 4th Seminar/functions/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+const serviceAccount = require(".wesopt29-server-1cc07-firebase-adminsdk-593u6-a1fad56666");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
